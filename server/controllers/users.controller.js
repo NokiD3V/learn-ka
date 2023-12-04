610fc2645fbb46651dabe290b4bb273587ff050e
NokiD3V (Nikita) <d3vnoki@gmail.com>
2023-12-04 22:30:19 +0500

    chore: Сделан контроллер регистрации

--- server/controllers/users.controller.js
+++ server/controllers/users.controller.js
@@ -1,3 +1,23 @@
-module.exports.register = () => {
+const logger = require('log4js').getLogger()
+logger.level = 'debug'
+const { validationResult } = require('express-validator');
+const ApiError = require('../exceptions/api-error');
+const UserService = require('../service/user.service');
 
+
+module.exports.register = async (req, res, next) => {
+    try {
+        const errors = validationResult(req)
+        if(!errors.isEmpty()){
+            return next(ApiError.BadRequest('Ошибка при валидации данных пользователя', errors.array()))
+        }
+        const {email, password} = req.body;
+        const userData = await UserService.register(email,password)
+
+        res.cookie('token', userData.token, {maxAge: 7*24*60*60*1000, httpOnly:true})
+        return res.json(userData)
+    } catch (e) {
+        next(e);
+        logger.debug(typeof e)
+    }
 }
