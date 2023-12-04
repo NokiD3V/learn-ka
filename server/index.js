a4c9c5717d2ca949472e79f968e3ad5b136f5854
NokiD3V (Nikita) <d3vnoki@gmail.com>
2023-12-04 22:24:01 +0500

    feat: Добавлена интеграция парсеров данных

--- server/index.js
+++ server/index.js
@@ -9,9 +9,25 @@ logger.level = 'debug'
 const express = require('express')
 const app = express()
 
-app.use('/api/users', require('./routers/user.router'))
+const bodyParser = require('body-parser')
+const cookieParser = require('cookie-parser')
+const cors = require("cors");
+const corsOptions = {
+  origin: "http://localhost:3000",
+  credentials:true
+};
 
-// Database connection 
+
+// Интеграция API вместе с парсерами
+app.use(cors(corsOptions));
+app.use(express.json({limit:"50mb"}));
+app.use(express.urlencoded({ extended: true, limit:"50mb", parameterLimit: 50000 }));
+app.use(bodyParser.json({ limit: "50mb" }))
+app.use(cookieParser())
+
+require("./routers/user.router")(app);
+
+// Подключение к базе данных, импорт основных моделей и проверки соеденения
 const db = require("./models");
 db.sequelize.sync()
 .then(() => {
