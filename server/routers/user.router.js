903cd0ed6eafbaae3253a0b35d841b7e6277a621
NokiD3V (Nikita) <d3vnoki@gmail.com>
2023-12-04 22:29:57 +0500

    chore: Изменена система роутеров, теперь путь находится в файле. Добавлен путь /api/users/register

--- server/routers/user.router.js
+++ server/routers/user.router.js
@@ -1,8 +1,17 @@
-const express = require('express')
-const router = express.Router()
+module.exports = app => {
+  const router = require('express').Router()
 
-router.get('/user', (req, res, next) => {
-  res.send({ success: true, message: 'A very cool message' }).status(200).end()
-})
+  const usersController = require('../controllers/users.controller')
 
-module.exports = router
+  const {body} = require('express-validator')
+
+  // URL: /api/users/...
+  router.post(
+    '/register',
+    body('email').isEmail(),
+    body('password').isLength({min:6, max:32}),
+    usersController.register
+  )
+
+  app.use('/api/users', router);
+}
