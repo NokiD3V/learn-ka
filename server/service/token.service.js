5e6e2dca93ef1fb18570ea7fd455c91fe38ad351
NokiD3V (Nikita) <d3vnoki@gmail.com>
2023-12-05 22:41:47 +0500

    feat: Добавлена проверка токена на валидность

--- server/service/token.service.js
+++ server/service/token.service.js
@@ -3,8 +3,17 @@ const jwt = require('jsonwebtoken')
 class TokenService {
     async generateToken(payload){
         const token = jwt.sign(payload, process.env.JWT_SECRET, {expiresIn:'7d'})
-        console.log("TOKEN", token)
         return token;
+    }
+
+    async validateToken(token){
+        try {
+            // Проверка токена на его валидность
+            const userData = await jwt.verify(token, process.env.JWT_SECRET)
+            return userData;
+        } catch (error) {
+            throw error
+        }
     }
 }
 
