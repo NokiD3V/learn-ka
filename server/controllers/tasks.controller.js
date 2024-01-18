30412b4d4ef00057b8d2754cd4cefbaccb4e4b88
NokiD3V (Nikita) <d3vnoki@gmail.com>
2024-01-19 00:45:35 +0500

    feat: добавлена JWT авторизация. Создан контроллер управлений заданий (генерация случайного задания)

---
+++ server/controllers/tasks.controller.js
@@ -0,0 +1,21 @@
+const logger = require('log4js').getLogger()
+logger.level = 'debug'
+const ApiError = require('../exceptions/api-error');
+const UserService = require('../service/user.service');
+const taskService = require('../service/task.service');
+
+
+module.exports.generateTask = async (req, res, next) => {
+    try {
+        console.log(req.userData)
+        const task = await taskService.getRandomTask(req.userData.class)
+        if(!task) return ApiError.BadRequest("Ошибка системы. Повторите попытку позже")
+
+        UserService.setTask(req.userData.id, task.id)
+
+        res.send({success: true, task: task}).status(200)
+    } catch (e) {
+        next(e);
+        logger.debug(typeof e)
+    }
+}
--- server/controllers/users.controller.js
+++ server/controllers/users.controller.js
@@ -3,6 +3,9 @@ logger.level = 'debug'
 const { validationResult } = require('express-validator');
 const ApiError = require('../exceptions/api-error');
 const UserService = require('../service/user.service');
+const tokenService = require('../service/token.service');
+const UserDto = require('../dtos/user.dto');
+const userService = require('../service/user.service');
 
 
 module.exports.register = async (req, res, next) => {
@@ -14,6 +17,9 @@ module.exports.register = async (req, res, next) => {
         const {email, password} = req.body;
         const userData = await UserService.register(email,password)
 
+        console.log(userData)
+
+        // Сохранение в куки пользователя токен для авторизации
         res.cookie('token', userData.token, {maxAge: 7*24*60*60*1000, httpOnly:true})
         return res.json(userData)
     } catch (e) {
@@ -23,13 +29,33 @@ module.exports.register = async (req, res, next) => {
 }
 
 module.exports.login = async (req, res, next) => {
-    try {
-        const {email, password} = req.body;
-        const userData = await UserService.login(email, password)
+    try{
+      const {email, password} = req.body;
+      const userData = await UserService.login(email, password)
+      res.cookie('refreshToken', userData.refreshToken, {maxAge: 7*24*60*60*1000, httpOnly:true})
+      return res.json(userData) 
+    } catch(e){
+      next(e)
+    }
+}
 
-        res.cookie('token', userData.token, {maxAge: 7*24*60*60*1000, httpOnly:true})
-        return res.json(userData)
-    } catch (e) {
+exports.logout = async (req, res, next) => {
+    try{
+      res.clearCookie('token')
+      return res.json({success: true})
+    } catch(e){
+      next(e)
+    }
+}
+  
+
+exports.refresh = async (req, res, next) => {
+    try{
+      const {refreshToken} = req.cookies;
+      const userData = await UserService.refresh(refreshToken)
+      res.cookie('refreshToken', userData.refreshToken, {maxAge: 7*24*60*60*1000, httpOnly:true})
+      return res.json(userData) 
+    } catch(e){
         next(e)
     }
 }
